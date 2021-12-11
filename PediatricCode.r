9ff3032f2ddc2f36915616a069a07439d450164e
rferrisx <rferrisx@gmail.com>
2021-12-10 19:07:00 -0800

    Update PediatricCode.r

--- PediatricCode.r
+++ PediatricCode.r
@@ -1,6 +1,6 @@
 # VAERS db code to pull out 'pediatric' AEs 
 # 2:00 PM 11/30/2021 -RMF
-# R 4.0.2 "Microsoft Open' or R 4.1.2 plus latest data.table
+# R 4.0.2 "Microsoft Open' or R 4.1.2 plus install latest data.table
 
 library(data.table)
 library(lubridate)
@@ -9,8 +9,7 @@ library(stringi)
 # Can use Header_Revised.r code for data import. See
 # Header_code_revised_import.r : https://github.com/rferrisx/VAERS/blob/main/HeaderCode_revised_import.r
 
-
-# Functions
+# Generic Functions
 `%nilike%` <- Negate(`%ilike%`) # e.g. 'not like'
 fsum <- function(x) {base::sum(x,na.rm=TRUE)}
 
@@ -26,7 +25,7 @@ stack.symptom.terms <<- stack.symptom.terms[,
 }
 
 # This query gets the age group but also picks some newborns suffering injury from 
-# being nursed vaccinated mothers. Some misplaced older (age) injuries. It does eliminate
+# being nursed by vaccinated mothers. Some misplaced older (age) injuries. It does eliminate
 # adverse events with phrase 'inappropriate age' in the SYMPTOM* fields:
 
 # setkey(all.data,"VAERS_ID_enhanced")
@@ -98,6 +97,7 @@ Under12[,.N,.(All_symptoms)][order(-N)][1:40]
 stack.symptoms(Under12)
 stack.symptom.terms[,.N,.(terms)][order(-N)][1:40]
 stack.symptom.terms[nchar(terms) > 10,.N,.(terms)][order(-N)][1:40]
+
 Under12[VAX_TYPE == "COVID19" & !duplicated(VAERS_ID_enhanced),
 .(Death=fsum(DIED == "Y"),
 L_THREAT=fsum(L_THREAT == "Y"),
@@ -188,7 +188,36 @@ final <- merge(setnames(as.data.table(AE.report,keep.rownames=TRUE),
 final[c(4:5,6:14,1:3)][c(2:8,12:14,9:11)][]
 #final[]
 }
-# END FUNCTION
 
 x <- Under12;CritMarks()
 x <- all.data[VAX_TYPE == "COVID19" & !duplicated(VAERS_ID_enhanced),];CritMarks()
+
+all.vaers.data[data.table::between(AGE_YRS,5,11) &
+	 data.table::between(CAGE_YR,5,11) &
+	 mdy(VAX_DATE) > mdy('10/31/2021') &
+	 !grepl("inappropriate age",All_symptoms) &
+	 VAX_TYPE == "COVID19",.N,.(SEX,AGE=as.integer(AGE_YRS))][
+	,dcast(.SD,AGE ~ SEX,value.var="N",fun.aggregate=fsum)]
+
+all.vaers.data[data.table::between(AGE_YRS,5,11) &
+	 data.table::between(CAGE_YR,5,11) &
+	 mdy(VAX_DATE) > mdy('10/31/2021') &
+	 !grepl("inappropriate age",All_symptoms) &
+	 VAX_TYPE == "COVID19",.N,.(SEX,AGE=as.integer(AGE_YRS),VAX_MANU)][
+	,dcast(.SD,AGE ~ VAX_MANU ,value.var="N",fun.aggregate=fsum)]
+
+
+# For reference 12 - 18
+all.vaers.data[data.table::between(AGE_YRS,12,18) &
+	 data.table::between(CAGE_YR,12,18) &
+	 mdy(VAX_DATE) > mdy('3/31/2021') &
+	 !grepl("inappropriate age",All_symptoms) &
+	 VAX_TYPE == "COVID19",.N,.(SEX,AGE=as.integer(AGE_YRS))][
+	,dcast(.SD,AGE ~ SEX,value.var="N",fun.aggregate=fsum)]
+
+all.vaers.data[data.table::between(AGE_YRS,12,18) &
+	 data.table::between(CAGE_YR,12,18) &
+	 mdy(VAX_DATE) > mdy('3/31/2021') &
+	 !grepl("inappropriate age",All_symptoms) &
+	 VAX_TYPE == "COVID19",.N,.(SEX,AGE=as.integer(AGE_YRS),VAX_MANU)][
+	,dcast(.SD,AGE ~ VAX_MANU ,value.var="N",fun.aggregate=fsum)]
