b2e3b76214a1e8a45d2137e25c70ebf2fad3a254
rferrisx <rferrisx@gmail.com>
2022-08-17 16:19:15 -0700

    Add files via upload

--- HeaderCode_revised_import.r
+++ HeaderCode_revised_import.r
@@ -1,11 +1,12 @@
-# Updated 7:51 AM 4/21/2022
-# Includes new db 'all.vaers.data' and 'all.data'
+# Updated 10:36 AM 7/29/2022
+# Includes new db 'all.vaers.data','all.data', all.covid.data
 # Includes memory management routines
 # Includes symptom and lexical summary analysis tables
 
 library(data.table)
-setDTthreads(0)
+setDTthreads(restore_after_fork = TRUE, percent = 80, throttle = 4096)
 library(lubridate)
+library(stringi)
 # Needs unzipped VAERS library from:
 # https://vaers.hhs.gov/data/datasets.html
 # Below is full 2020 data and 2021 data through November
@@ -19,7 +20,7 @@ fsum <- function(x) {base::sum(x,na.rm=TRUE)}
 
 ## DATA IMPORT routines
 # merge routines:
-# All 2020 data. Note: You can simply download the yearly 2020 data
+# All 2020 data
 setwd("D:\\Politics\\VAERS\\2020VAERSData.2020_07.19.2021")
 Data_Vax <- merge(fread("2020VAERSDATA.csv"),fread("2020VAERSVAX.csv"),all.x=TRUE,by="VAERS_ID")
 setkey(Data_Vax,VAERS_ID)
@@ -34,8 +35,8 @@ Data_Vax_SYMP_2020 <- Data_Vax_SYMP[order(VAERS_ID,-DIED,-VAX_DOSE_SERIES)]
 mergeDVS <- Data_Vax_SYMP[!duplicated(VAERS_ID),]
 mergeDVS2020 <- mergeDVS
 
-# 2021 data Note: You can simply download the yearly 2021 data
-setwd("D:\\Politics\\VAERS\\2021VAERSData.01.07.2022") 
+# 2021 data
+setwd("D:\\Politics\\VAERS\\2021VAERSData.01.07.2022")
 Data_Vax <- merge(fread("2021VAERSDATA.csv"),fread("2021VAERSVAX.csv"),all.x=TRUE,by="VAERS_ID")
 setkey(Data_Vax,VAERS_ID)
 Data_Vax_SYMP <- merge(Data_Vax,fread("2021VAERSSYMPTOMS.csv"),all.x=TRUE,by="VAERS_ID")[
@@ -50,8 +51,8 @@ mergeDVS <- Data_Vax_SYMP[!duplicated(VAERS_ID),]
 mergeDVS2021 <- mergeDVS
 
 
-# 2022 data Note: You can simply download the yearly 2021 data
-setwd("D:\\Politics\\VAERS\\2022VAERSData.04.15.2022")
+# 2022 data
+setwd("D:\\Politics\\VAERS\\2022VAERSData.08.16.2022")
 Data_Vax <- merge(fread("2022VAERSDATA.csv"),fread("2022VAERSVAX.csv"),all.x=TRUE,by="VAERS_ID")
 setkey(Data_Vax,VAERS_ID)
 Data_Vax_SYMP <- merge(Data_Vax,fread("2022VAERSSYMPTOMS.csv"),all.x=TRUE,by="VAERS_ID")[
@@ -86,7 +87,7 @@ all.vaers.data[VAX_MANU %in% c("MODERNA","PFIZER\\BIONTECH","JANSSEN","UNKNOWN M
  VAX_TYPE == "COVID19",][!duplicated(VAERS_ID_enhanced_vaers),.N]
 all.covid.data <- all.vaers.data[VAX_MANU %in% c("MODERNA","PFIZER\\BIONTECH","JANSSEN","UNKNOWN MANUFACTURER") &
  VAX_TYPE == "COVID19",][!duplicated(VAERS_ID_enhanced_vaers),]
-save(all.covid.data, file="all.covid.data.RDS")
+saveRDS(all.covid.data, file="all.covid.data.RDS")
 
 
 all.data <- rbind(Data_Vax_SYMP_2020,Data_Vax_SYMP_2021,Data_Vax_SYMP_2022)
@@ -109,13 +110,16 @@ all.data[VAX_TYPE == "COVID19",.N]
 all.data[VAX_TYPE == "COVID19" & !duplicated(VAERS_ID_enhanced),.N]
 all.data[VAX_TYPE == "COVID19" & !duplicated(VAERS_ID_enhanced),
 .(Death=fsum(DIED == "Y"),L_THREAT=fsum(L_THREAT == "Y"),HOSPITAL=fsum(HOSPITAL =="Y"),HOSPITAL.DAYS=fsum(HOSPDAYS))]
+dupVAERS <- all.covid.data[DIED == "Y",][duplicated(VAERS_ID),.(VAERS_ID)]
+all.covid.data[DIED == "Y",][VAERS_ID %in% dupVAERS$VAERS_ID,
+.(VAERS_ID,RECVDATE=mdy(RECVDATE),All_symptoms,VAERS_ID_enhanced)][order(RECVDATE)][1:100]
 #END DATA IMPORT routines
 
 # memory management
 # deletes intermediate files
 # runs gc()
-
-memory.size()
+rver <- as.numeric(substr(paste0(as.matrix(version)[7],".",as.matrix(version)[8]),0,3))
+if(rver < 4.2) {memory.size()}
 # to free up memory
 rm( list=c("Data_Vax",
 "Data_Vax_SYMP",
@@ -126,7 +130,7 @@ rm( list=c("Data_Vax",
 "mergeDVS2021",
 "mergeDVS2022"))
 gc();gc();gc();
-memory.size()
+if(rver < 4.2) {memory.size()}
 
 # list memory
 as.data.table(sapply(ls(),
@@ -158,6 +162,7 @@ AE.report <- rbind(
 	"HOSPITAL",
 	"HOSPITAL.DAYS",
 	"ER_ED_VISIT")))],
+
 
 # critical demographics
 	t(x[
@@ -193,6 +198,7 @@ AE.Died.report <- rbind(
 	"HOSPITAL",
 	"HOSPITAL.DAYS",
 	"ER_ED_VISIT")))],
+
 
 # critical demographics
  t(Died[VAX_TYPE == "COVID19",lapply(.SD[
@@ -274,21 +280,37 @@ l <- l[,setnames(as.data.table(stringi::stri_trans_tolower(terms)),"V1","terms")
 stack.symptom.terms <<- l
 }
 
-# run string functions
 
+# run "All_symptoms" string functions
+
+print("All_symptoms for all injuries")
 stack.symptoms(all.covid.data)
 # list top 30 terms nchar < 10
 stack.symptom.terms[nchar(terms) < 10,.N,.(terms)][order(-N)][1:30]
 # list top 30 terms nchar >= 10
 stack.symptom.terms[nchar(terms) > 10,.N,.(terms)][order(-N)][1:30]
 
+# list top 30 terms nchar < 13
+stack.symptom.terms[nchar(terms) < 13,.N,.(terms)][order(-N)][1:30]
+# list top 30 terms nchar >= 13
+stack.symptom.terms[nchar(terms) > 13,.N,.(terms)][order(-N)][1:30]
+
+print("DIED == 'Y'")
 stack.symptoms(all.covid.data[DIED == "Y",])
 # list top 30 terms nchar < 10
 stack.symptom.terms[nchar(terms) < 10,.N,.(terms)][order(-N)][1:30]
 # list top 30 terms nchar >= 10
 stack.symptom.terms[nchar(terms) > 10,.N,.(terms)][order(-N)][1:30]
 
-cat(' Working on this:
+# list top 30 terms nchar < 13
+stack.symptom.terms[nchar(terms) < 13,.N,.(terms)][order(-N)][1:30]
+# list top 30 terms nchar >= 13
+stack.symptom.terms[nchar(terms) > 13,.N,.(terms)][order(-N)][1:30]
+
+
+
+
+cat('
 stack.symptom.text(all.covid.data)
 # list top 30 terms nchar < 10
 stack.symptom.terms[nchar(terms) < 10,.N,.(terms)][order(-N)][1:30]
@@ -304,6 +326,14 @@ stack.symptom.terms[nchar(terms) > 10,.N,.(terms)][order(-N)][1:30]
 
 all.covid.data[,.N,.(All_symptoms)][order(-N)][1:40]
 all.covid.data[DIED == "Y",.N,.(All_symptoms)][order(-N)][1:40]
+
+
+
+all.covid.data[!duplicated(VAERS_ID) &
+ stri_detect_regex(All_symptoms,"COVID-19 Drug",case_insensitive=TRUE),.(substr(SYMPTOM_TEXT,0,220))]
+
+
+
 
 
 
